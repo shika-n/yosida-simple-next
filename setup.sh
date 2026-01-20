6de83e303f480d4728d351a345aac1d962246d55
Shika <greffinroid@ymail.com>
2026-01-20 15:22:58 +0900

    Fix setup folder typo

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 mkdir tmp
 curl ftp://ftp.edrdg.org/pub/Nihongo//JMdict_e.gz --output tmp/JMdict_e.gz
 gzip -d tmp/JMdict_e.gz && mv tmp/JMdict_e tmp/JMdict_e.xml
-python script/preprocess.py
+python scripts/preprocess.py
