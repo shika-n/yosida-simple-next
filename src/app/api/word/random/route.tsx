248e36c50d65f542ce89f08b0e740da51ef41954
Shika <greffinroid@ymail.com>
2026-01-22 10:20:20 +0900

    Fix search params and add isIdOnly parameter

--- src/app/api/word/random/route.tsx
+++ src/app/api/word/random/route.tsx
@@ -5,7 +5,11 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
 	const params = request.nextUrl.searchParams;
 
-	const word = getRandomWord(params.get("common") ? true : false);
+	const isCommon = Number(params.get("common")) === 1;
+	const isIdOnly = Number(params.get("idOnly")) === 1;
+
+	const word = getRandomWord(isCommon);
+
 	if (!word) {
 		return NextResponse.json(
 			{ message: "This shouldn't happen. Is the database empty?" },
@@ -15,5 +19,8 @@ export async function GET(request: NextRequest) {
 
 	const glossaries = getGlossariesOf(word);
 
+	if (isIdOnly) {
+		return NextResponse.json({ word: { id: word.id } });
+	}
 	return NextResponse.json({ word, glossaries });
 }
